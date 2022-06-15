3e0ab095f66026b3681d767e9156698cfebdf375
jeffersondrs <jeffersonduarte@id.uff.br>
2022-06-15 11:27:28 -0300

    javascript complete

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const listaUl = document.getElementById('lista');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    addLista()
+})
+
+function addLista(lista) {
+    let listaText = input.value;
+
+    if(lista){
+        listaText = lista.text;
+    }
+    if(listaText){
+        const listaEL = document.createElement('li');
+        if(lista && lista.completed){
+            listaEL.classList.add('completed');
+        }
+        listaEL.innerText = listaText;
+        listaEL.addEventListener('click', () => listaEL.classList.toggle('completed'));
+        listaEL.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            listaEL.remove();
+        });
+
+        listaUl.appendChild(listaEL);
+        input.value = '';
+
+    console.log(listaText);
+}}
